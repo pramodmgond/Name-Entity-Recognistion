4a2dfb9c01d685330df50866ffb370709646e74d
pramod <pramodgond0075@gmail.com>
2024-07-11 23:28:10 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="pramod",
+    author_email="pramodagond@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
